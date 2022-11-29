a9bc65681049400486be3b3c795ffc529ac0c539
August Halverson <augusthalverson@gmail.com>
2022-11-29 16:04:26 -0600

    feat: add new test component

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,11 @@
+import type { FC } from 'react';
+import React from 'react';
+import { Text } from 'react-native';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
+
+export const TestComponent: FC = () => {
+  return <Text>Test</Text>;
+};
